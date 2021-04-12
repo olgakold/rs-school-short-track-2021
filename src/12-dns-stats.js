f6e5d364d7a89a44986a4e55e9eb38ff01ac4feb
finuser4 <finuser4@belkraft.local>
2021-04-12 14:10:53 +0300

    add task 12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,26 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains ) { let maxItem = domains[0];
+  for (let i=0; i<domains.length-1; i++) {
+      domains[i].length > domains[i+1].length ? maxItem = domains[i] : maxItem = domains[i+1]
+  }    
+  let items = []
+  maxItemArr =  maxItem.split('.').reverse()
+  let itemsName = ''
+  for (let i=0; i<maxItemArr.length; i++){
+      itemsName = `${itemsName}.${maxItemArr[i]}`
+      items.push(itemsName) 
+  }
+  let result = {}
+  for (let i=0; i<items.length; i++){
+      let sum = 0;
+      let reg = new RegExp(items[i].split('.').reverse().join('.').slice(0, -1));
+      domains.map(elem => elem.match(reg) ? sum++ : '')
+      result[items[i]] = sum
+  }   
+
+  return result
 }
 
 module.exports = getDNSStats;
